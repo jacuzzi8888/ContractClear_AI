f01f03fe5218b192c7134a5e128e4708265ecd95
jacuzzi8888 <coolguyben126@gmail.com>
2026-04-07 05:58:19 +0100

    Add debugging to proxy

--- app/src/proxy.ts
+++ app/src/proxy.ts
@@ -1,7 +1,13 @@
 import { auth0 } from "./lib/auth0";
 
 export async function proxy(request: Request) {
-  return await auth0.middleware(request);
+  const url = new URL(request.url);
+  console.log("[proxy] Request:", url.pathname);
+  
+  const response = await auth0.middleware(request);
+  
+  console.log("[proxy] Response status:", response.status);
+  return response;
 }
 
 export const config = {
